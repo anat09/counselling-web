06572a2070e5680c23672b8a9a6fd43beabadf6f
Tommy Smith <tommy.smith023@gmail.com>
2022-03-09 22:46:05 +0000

    attempt fix of text color in mobile dark mode

--- src/components/section.js
+++ src/components/section.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 const Section = ({ paras }) => {
   return (
     <section className="font-lato p-2">
-      {paras.map((para, idx) => <p key={idx} className="leading-8 py-4 text-lg sm:py-1.5">{para}</p>)}
+      {paras.map((para, idx) => <div key={idx} className="leading-8 py-4 text-lg sm:py-1.5">{para}</div>)}
     </section>
   )
 }
