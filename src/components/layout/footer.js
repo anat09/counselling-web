545c3f8e9d11fe12a58025f9aa49fa48a9ef1797
Tommy Smith <tommy.smith023@gmail.com>
2022-03-26 17:27:13 +0000

    fix footer links

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -3,9 +3,9 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <div className="bg-white text-primary flex font-lato font-bold text-2xl py-4 justify-center">
-      <Link href='/privacy'><a>Privacy</a></Link>
+      <Link href='/privacy#header'><a>Privacy</a></Link>
       <div className="px-6" />
-      <Link href='/disclaimer'><a>Disclaimer</a></Link>
+      <Link href='/disclaimer#header'><a>Disclaimer</a></Link>
     </div>
   )
 }
