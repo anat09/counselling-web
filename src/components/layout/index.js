b4f897cb829c696f1720654553e34d04efea9f0d
Tommy Smith <tommy.smith023@gmail.com>
2022-03-31 15:50:36 +0100

    Update index.js

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -34,7 +34,9 @@ export default function Layout({ children }) {
       <div className="z-10">
         <Navbar/>
       </div>
-      <CoverImage />
+      <div className="z-0">
+        <CoverImage />
+      </div>
       <div id="header" className="max-w-2xl px-4 py-6 sm:px-10 sm:py-10 mx-auto bg-primary text-white text-center text-xl sm:text-3xl md:text-4xl font-lato sm:rounded-lg shadow-md  lg:-translate-y-44 md:-translate-y-20">
         Counselling and Psychotherapy from Greater Manchester and Cheshire
       </div>
