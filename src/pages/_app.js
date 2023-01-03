afe0a3952a84d510339bf56cd0b82af4fc2fed82
Tommy Smith <tommy.smith023@gmail.com>
2023-01-03 19:07:30 +0000

    add vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,14 @@ import '../styles/index.css'
 import Container from '../components/container'
 import Layout from '../components/layout'
 // import 'flowbite'
-
+import { Analytics } from '@vercel/analytics/react'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Container>
         <Component {...pageProps} />
+        <Analytics />
       </Container>
     </Layout>
   )
