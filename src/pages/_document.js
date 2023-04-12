995785da202915acd3bd924150557336a932e299
Tommy Smith <tommy.smith023@gmail.com>
2023-04-12 21:00:17 +0100

    add title and meta tags to head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,11 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta charSet="utf-8" />
+          <title key="title">Anamaria Tigan - Counselling in Greater Manchester & Cheshire</title>
+          <meta charSet="utf-8">
+            Anamaria Tigan offers face-to-face & remote counselling in Greater Manchester & Cheshire,
+            providing a safe space for personal growth & healing. Book a consultation today.
+          </meta>
           <link rel="shortcut icon" href="/favicons/favicon.ico" />
         </Head>
         <body>
