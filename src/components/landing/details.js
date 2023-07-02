934ebf5f71ebf5c8563ee266a225091c26d7f2cf
Tommy Smith <tommy.smith023@gmail.com>
2023-07-02 13:54:11 +0100

    rearrange logos

--- src/components/landing/details.js
+++ src/components/landing/details.js
@@ -3,22 +3,26 @@ import React from 'react';
 import Icons from './icons';
 import AtLogo from './atLogo';
 import BACPLogo from './bacpLogo';
-import MNCSLogo from './mncsLogo';
+import MNCPSLogo from './mncpsLogo';
 
 export default function Details() {
   return (
-    <div className="flex sm:flex-row flex-col justify-evenly items-center">
-      <div className="sm:mt-0 mt-6">
-        <MNCSLogo />
+    <div>
+      <div className="flex sm:flex-row flex-col justify-evenly items-center">
+        <div className="sm:mt-0 mt-10">
+            <AtLogo /> 
+        </div>
+        <div className="sm:mt-0 mt-10">
+          <Icons />
+        </div>
+        <div className="sm:mt-0 mt-10">
+          <BACPLogo />
+        </div>
       </div>
-      <div>
-        <AtLogo /> 
-      </div>
-      <div>
-        <Icons />
-      </div>
-      <div className="sm:mt-0 mt-10">
-        <BACPLogo />
+      <div className="flex sm:flex-row flex-col justify-evenly items-center">
+        <div className="sm:mt-0 mt-6">
+          <MNCPSLogo />
+        </div>
       </div>
     </div>
   )
