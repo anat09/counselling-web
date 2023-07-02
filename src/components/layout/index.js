4b095766a5c55897ed0857924926897f038ac0bd
Tommy Smith <tommy.smith023@gmail.com>
2023-07-02 13:54:46 +0100

    July content changes (#7)
    * changing landing content for seo
    
    * change header bubble
    
    * add mncps logo
    
    * rearrange logos

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -37,7 +37,7 @@ export default function Layout({ children }) {
         <CoverImage />
       </div>
       <div id="header" className="max-w-2xl px-4 py-6 sm:px-10 sm:py-10 mx-auto bg-primary text-white text-center text-xl sm:text-3xl md:text-4xl font-lato sm:rounded-lg shadow-md lg:-translate-y-48 md:-translate-y-24">
-        Counselling and Psychotherapy from Greater Manchester and Cheshire
+        Counselling and Psychotherapy for Individuals and Couples <br/> Delivered Online and In-Person
       </div>
       <Container>
         <main className={styles.main}>{children}</main>
