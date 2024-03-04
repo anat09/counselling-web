9add05ebaa85262c639aa2cbe27b0ec98e266bb2
Tommy Smith <tommy.smith023@gmail.com>
2024-03-04 19:54:45 +0000

    Fix footer links

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,13 +4,13 @@ const Footer = () => {
   return (
     <div className="bg-white text-primary flex flex-wrap flex-grow font-lato font-bold text-2xl py-4 justify-around">
       <div className="m-2">
-        <Link href='/privacy#header'><a>Privacy</a></Link>
+        <Link href='/privacy#title'><a>Privacy</a></Link>
       </div>
       <div className="m-2">
-        <Link href='/disclaimer#header'><a>Disclaimer</a></Link>
+        <Link href='/disclaimer#title'><a>Disclaimer</a></Link>
       </div>
       <div className="m-2">
-        <Link href='/romana#header'><a>În Limba Română</a></Link>
+        <Link href='/romana#title'><a>În Limba Română</a></Link>
       </div>
     </div>
   )
